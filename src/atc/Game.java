57572ebcb0a84ccb2493d482e9235fd146d6f942
pedro-cruz <menesescruz.pedro@gmail.com>
2025-10-28 16:19:14 -0300

    Mudanças principais - pronto para codigo dos atos

--- src/atc/Game.java
+++ src/atc/Game.java
@@ -1,36 +1,117 @@
 package atc;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Game {
 
+	// ---------------------------------------------------------------------
+	// INICIALIZANDO O SISTEMA
 	public static void main(String[] args) {
 
 		int tempo = 0;
+		int option;
+		int playerTurn;
+		int enemyTurn;
+		int enemyOption;
 
 		Scanner input = new Scanner(System.in);
-
-		System.out.println("Selecione a dificuldade: \nNormal. \nDificil.");
+		Random generator = new Random();
+		
+		// ---------------------------------------------------------------------
+		// CONFIGURANDO O JOGO
+		
+		//selecao de dificuldade
+		System.out.println("Selecione a dificuldade: \n" + "Fácil. \n" + "Normal. \n" + "Dificil.");
+		System.out.print("R: ");
 		String difficulty = input.nextLine().toUpperCase();
+		Player player = new Player();
 
-		while (tempo != 3 && tempo != 4) {
+		while (tempo == 0) {
 
-			if (difficulty.startsWith("N")) { // define a dificuldade para normal
-				Player player = new Player(100); // mais vida de jogador
-				tempo = 4; // mais tempo
+			if (difficulty.startsWith("F")) {
+				player.hp = 120; // mais vida de jogador
+				tempo = 5; // mais tempo
+				difficulty = "Fácil";
+
+			} else if (difficulty.startsWith("N")) { // define a dificuldade para normal
+				player.hp = 100; // vida de jogador padrao
+				tempo = 4; // tempo padrao
+				difficulty = "Normal";
 
 			} else if (difficulty.startsWith("D")) { // define a dificuldade para dificil
-				Player player = new Player(80); // menos vida de jogador
+				player.hp = 80; // menos vida de jogador
 				tempo = 3; // menos tempo
+				difficulty = "Difícil";
 			} else {
 				System.out.println("Insira uma dificuldade válida");
+				System.out.print("R: ");
 				difficulty = input.nextLine().toUpperCase();
 			}
 		}
+		System.out.println("Dificildade escolhida: " + difficulty);
 
-		System.out.println("Selecione a classe: \nVelocidade. \\nForça. \nInteligência.");
-		String playerClass = input.nextLine().toUpperCase();
 		
+		//seleção de classe
+		System.out.println("Escolha: \n" + "Força: Par de braços \n" + "Velocidade: Prótese inferior \n"
+				+ "Inteligência: Implante cervical");
+		System.out.print("R: ");
+		String playerClass = input.nextLine().toUpperCase();
+
+		do {
+
+			if (playerClass.startsWith("V")) {
+				player.playerClass = "Velocidade";
+				player.setPlayerClass("Velocidade");
+				// chama o método de definir classe, para garantir que não seja apenas um
+				// valorzinho na variável
+			} else if (playerClass.startsWith("F")) {
+				player.playerClass = "Forca";
+				player.setPlayerClass("Forca");
+
+			} else if (playerClass.startsWith("I")) {
+				player.playerClass = "Inteligencia";
+				player.setPlayerClass("Inteligencia");
+
+			} else {
+				System.out.println("Insira uma opção válida!");
+				System.out.print("R: ");
+				playerClass = input.nextLine().toUpperCase();
+			}
+		} while (!playerClass.startsWith("V") && !playerClass.startsWith("F") && !playerClass.startsWith("I"));
+
+		System.out.println("Opção escolhida: " + player.playerClass);
+		System.out.println("Seja bem vindo ao nosso jogo. ");
+		System.out.println(" ");
+
+//--------------------------------------------------------------------- COMEÇANDO O JOGO 
+
+		//ATO 1
+		System.out.println("Como P2 deve tentar invadir? \n"
+				+ "1. Subir a escada. \n"
+				+ "2. Forçar o portão \n"
+				+ "3. Consertar o sistema de controle");
+		option = input.nextInt();
+		
+		switch (option) {
+		
+		case 1:
+			tempo = tempo - 1;
+		break;
+		
+		case 2:
+			if (!player.playerClass.startsWith("F")) {
+				tempo = tempo-1;
+			} else {
+	
+			}
+			break;
+			
+		case 3:
+			break;
+		
+		
+		}
 		
 
 	}
--- src/atc/Player.java
+++ src/atc/Player.java
@@ -4,25 +4,138 @@ import java.util.Random;
 
 public class Player {
 
-	int vida; //vida do personagem
-	char playerClass; //classe do personagem
-	String status; //status do personagem
-	int hit;
-	int minHit = 1; //acerto minimo do dado
-	int maxHit = 20; //acerto maximo do dado
-	
+	int hp; // vida do personagem
+	String playerClass; // classe do personagem
+	String status; // status do personagem
+	int hit; //acerto ataque
+	int dodge; //acerto esquiva
+	int counter; //acerto contra ataque
+	int damage; //dano
+	int defense; //defesa do jogador
+
 	Random generator = new Random();
 	
 	Player(){
-		
 	}
 	
-	Player(int vida){ //construtor que definirá vida(definida pela dificuldade)
-		this.vida = vida;
-	}
-	
-	void attack() { //açao de ataque do jogador
-		hit = generator.nextInt(21); //sorteará um número de 0 a 20
+	Player(int hp){ //construtor que definirá vida(definida pela dificuldade)
+		this.hp = hp;
 	}
 
+	void setPlayerClass(String playerClass) { // definrá a classe do jogador
+		this.playerClass = playerClass.toUpperCase();
+	}
+
+	String getPlayerClass() {
+		return playerClass;
+	}
+
+	int getAttack() { // ação de ataque
+		hit = generator.nextInt(21); // sorteará um número de 0 a 20
+		
+		//erro critico
+		if (hit == 0) {
+			hit += 1;
+			return hit;
+			
+			//acerto critico
+		} else if (hit == 20) {
+			return hit;
+			
+			//evita criticos não naturais com bonus
+		} else if (hit == 15) {
+			hit += 6;
+			return hit;
+			
+			//retorno padrão, com bonus
+		} else {
+			hit += 5;
+			return hit;
+		}
+
+	}
+
+	int getDamage() { // ação de dano
+
+		// dano máximo: 33
+		damage = generator.nextInt(34);
+
+		// calcula o dano mínimo: 10
+		if (damage < 10) {
+			damage = 10;
+			
+			//calcula o bônus de força
+		} else if (playerClass.startsWith("F")) {
+			damage *= 2;
+		}
+		return damage;
+	}
+
+	int getDodge() { // ação de esquiva
+		dodge = generator.nextInt(21);
+
+		// erro crítico
+		if (dodge == 0) {
+			dodge += 1;
+			return dodge;
+
+			// acerto critico
+		} else if (dodge == 20) {
+			return dodge;
+
+			// contagem de bônus da classe
+		} else if (playerClass.startsWith("V")) {
+			dodge += 5;
+
+			// evita um falso acerto critico (20 não natural)
+			if (dodge == 20) {
+				dodge += 1;
+				return dodge;
+
+				// retorno do valor comum, com bonus e sem criticos
+			} else {
+				return dodge;
+			}
+
+			// retorno do valor comum, sem bonus e sem criticos
+		} else {
+			return dodge;
+		}
+	}
+
+	int getCounter() { // ação de contra-ataque
+		counter = generator.nextInt(21);
+
+		// erro crítico
+		if (counter == 0) {
+			counter += 1;
+			return counter;
+
+			// acerto critico
+		} else if (dodge == 20) {
+			return dodge;
+
+			// contagem de bônus da classe
+		} else if (playerClass.startsWith("I")) {
+			counter += 5;
+
+			// evita um falso acerto critico (20 não natural)
+			if (counter == 20) {
+				counter += 1;
+				return counter;
+
+				// retorno do valor comum, com bonus e sem criticos
+			} else {
+				return counter;
+			}
+
+			// retorno do valor comum, sem bonus e sem criticos
+		} else {
+			return counter;
+		}
+	}
+	
+
 }
+
+
