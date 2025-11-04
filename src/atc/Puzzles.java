2612353a8c51e9d930d82b0cdc23339d741a720d
pedro-cruz <menesescruz.pedro@gmail.com>
2025-11-03 22:05:23 -0300

    Puzzle 2 ainda quebrado

--- src/atc/Puzzles.java
+++ src/atc/Puzzles.java
@@ -19,24 +19,23 @@ public class Puzzles {
 			number = 2;
 		}
 
-		for (int n = 1; n >= 5; n++) {
-			number = number * n;
+		for (int n = 1; n <= 5; n++) {
+			number = number * 2;
 			System.out.println(number);
-			temp = n;
 		}
 
 		System.out.println("Qual o próximo número da sequência?");
-		number = number * (temp+1);
+		number = number*2;
 		return number;
 	}
-	
+
 	public static int puzzle2(int textSpeed) {
-		
+
 		TextUtils.slowPrint("Para estabilizar o sistema, P2 precisa calcular a energia total de um reator.\r\n"
 				+ "O terminal mostra:\r\n"
 				+ "“Três núcleos com potências crescentes — o segundo é o dobro do primeiro, e o terceiro é 3 unidades \n"
 				+ " acima do segundo. A soma deles é 33. Qual é o valor da potência do primeiro núcleo?”", textSpeed);
-		
+
 		System.out.print("R: ");
 		number = 6;
 		return number;
